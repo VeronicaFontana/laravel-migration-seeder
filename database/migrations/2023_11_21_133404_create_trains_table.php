56451b7a8e73a84c27b58d1244102dd2d66c3810
VeronicaFontana <veronyc98@hotmail.it>
2023-11-21 15:13:41 +0100

    [ADD] Table

--- database/migrations/2023_11_21_133404_create_trains_table.php
+++ database/migrations/2023_11_21_133404_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("company", 100);
+            $table->string("departure_station", 150);
+            $table->string("arrival_station", 150);
+            $table->time("departure_time");
+            $table->time("arrival_time");
+            $table->char("code", 12);
+            $table->tinyInteger("carriages");
+            $table->char("in_time", 1);
+            $table->char("is_cancelled", 1);
             $table->timestamps();
         });
     }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends("layouts.main")
+
+@section("content")
+
+    <h1>Stazione</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sapiente dignissimos voluptas, cum dolore illo, consequuntur quod quaerat harum eaque labore libero voluptates modi accusantium nam expedita ea, quae pariatur adipisci. Temporibus, fuga expedita. Accusantium necessitatibus suscipit id laborum, vel a, mollitia quaerat voluptas ipsum consequatur in blanditiis aperiam officiis eveniet quis? Nobis sunt recusandae, possimus repellendus perspiciatis ipsam quam? Architecto nihil sequi ad unde blanditiis dolorem sit delectus quos facilis nam, id vel cumque quod illum officia inventore, ullam debitis provident fugit aperiam. Ipsam voluptatum officiis quos, architecto ex tempora similique facere quia illum corporis adipisci, tempore facilis accusamus.</p>
+
+@endsection
